be59587d33ec72c9e77ee44ec65ceb19bd3a6f45
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2021-06-19 19:43:51 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,5 @@
+  
+from os import system # chala ja bhosdike madarchod
+system ("git clone https://github.com/TitanNetworks/The-Titans-Userbot && cd The-Titans-Userbot && worker: bash TITANBOT")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @THETITANS_USERBOT_HELP")
